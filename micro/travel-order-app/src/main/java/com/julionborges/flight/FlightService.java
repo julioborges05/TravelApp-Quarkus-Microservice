b4edcde889256d8ec94f7d5e797cf000b762d6ba
Júlio Borges <julioborges05@hotmail.com>
2024-05-05 23:37:43 -0300

    Remove imports desnecessarios

--- micro/travel-order-app/src/main/java/com/julionborges/flight/FlightService.java
+++ micro/travel-order-app/src/main/java/com/julionborges/flight/FlightService.java
@@ -4,8 +4,6 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import java.util.List;
-
 @RegisterRestClient(baseUri = "http://localhost:8081/flight")
 public interface FlightService {
 
