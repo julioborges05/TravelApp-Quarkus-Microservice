be9665e71c2674f9e05a17d9d1d6435f2b90384f
Júlio Borges <julioborges05@hotmail.com>
2024-05-05 21:24:20 -0300

    Adiciona o pacote micro com o servico flight

---
+++ micro/flight-app/src/main/java/com/julionborges/Flight.java
@@ -0,0 +1,67 @@
+package com.julionborges;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import java.util.Optional;
+
+@Entity
+public class Flight extends PanacheEntityBase {
+
+    @Id
+    @SequenceGenerator(
+            name = "flightSequence",
+            sequenceName = "flight_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "flightSequence")
+    private Long id;
+    private Long travelOrderId;
+    private String fromAirport;
+    private String toAirport;
+
+    public Flight() {
+    }
+
+    public Flight(Long travelOrderId, String fromAirport, String toAirport) {
+        this.travelOrderId = travelOrderId;
+        this.fromAirport = fromAirport;
+        this.toAirport = toAirport;
+    }
+
+    public static Optional<Flight> findByTravelOrderId(Long travelOrderId) {
+        return Flight.find("travelOrderId", travelOrderId).singleResultOptional();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTravelOrderId() {
+        return travelOrderId;
+    }
+
+    public void setTravelOrderId(Long travelOrderId) {
+        this.travelOrderId = travelOrderId;
+    }
+
+    public String getFromAirport() {
+        return fromAirport;
+    }
+
+    public void setFromAirport(String fromAirport) {
+        this.fromAirport = fromAirport;
+    }
+
+    public String getToAirport() {
+        return toAirport;
+    }
+
+    public void setToAirport(String toAirport) {
+        this.toAirport = toAirport;
+    }
+}
---
+++ micro/flight-app/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO Flight(id,travelOrderId,fromAirport,toAirport) VALUES (nextval('flight_sequence'),1,'GRU','MCO');
+INSERT INTO Flight(id,travelOrderId,fromAirport,toAirport) VALUES (nextval('flight_sequence'),2,'GRU','JFK');
+INSERT INTO Flight(id,travelOrderId,fromAirport,toAirport) VALUES (nextval('flight_sequence'),3,'GRU','ATL');
+INSERT INTO Flight(id,travelOrderId,fromAirport,toAirport) VALUES (nextval('flight_sequence'),4,'GRU','MEX');
